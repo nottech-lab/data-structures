9c0d45000445bfb737920b6dfa8ca89f979efb72
samwelnm4@gmail.com <samwelnm4@gmail.com>
2020-08-13 14:01:46 +0300

    added data-structure on arrays

---
+++ Arrays.py
@@ -0,0 +1,31 @@
+
+'''
+Data structure  challenge on Arrays
+'''
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def reverseArray(a):
+    temp = []
+    size = len(a)
+    for i in range(size-1, -1, -1):
+        temp.append(a[i])
+    return temp
+    if __name__ == '__main__':
+        fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+        arr_count = int(input())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        res = reverseArray(arr)
+
+        fptr.write(' '.join(map(str, res)))
+        fptr.write('\n')
+
+        fptr.close()
